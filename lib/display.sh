966fb3b8d6492b79b433ff3ca2393ebd99796093
gc-MayankPun <punmayank35@gmail.com>
2026-07-17 01:16:25 +0530

    Intial Commit

---
+++ lib/display.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+display_terminal(){
+    echo "Hello"
+}
---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+source "$ROOT/config.sh"
+source "$ROOT/lib/display.sh"
+
+display_terminal
