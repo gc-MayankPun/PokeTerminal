62fc7538ed67e6cb92ee5dde76bf48bd3c1d58b9
gc-MayankPun <punmayank35@gmail.com>
2026-07-26 03:20:22 +0530

    Refactor friendship bar logic: move draw_friendship_bar function to ui.sh and streamline its implementation

--- lib/utils.sh
+++ lib/utils.sh
@@ -9,21 +9,3 @@ chance() {
   local percent=$1
   ((RANDOM % 100 < $percent))
 }
- 
-draw_friendship_bar() {
-  local friendship=$1
-  local max_friendship=255
-  local total_blocks="${2:-9}"    
-
-  local percentage=$(( friendship * 100 / max_friendship ))
-  local filled=$(( friendship * total_blocks / max_friendship ))
-  (( filled == 0 && friendship > 0 )) && filled=1
-  (( filled > total_blocks )) && filled=$total_blocks
-  local empty=$(( total_blocks - filled ))
-
-  local bar=""
-  for (( i=0; i<filled; i++ )); do bar+="■"; done
-  for (( i=0; i<empty; i++ )); do bar+="□"; done
-
-  echo "$bar ${percentage}%"
-}
